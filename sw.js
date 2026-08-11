db3832ce9f249612a863aeb3e817ec24a3ab766f
Griff Gamble <griff@gm-charlie.com>
2026-08-11 18:17:20 +0800

    Dockit v34 bar logo, hidden utils, centered picker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v33';
+const CACHE = 'docket-v34';
 
 const SHELL = [
   './',
