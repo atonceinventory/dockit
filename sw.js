68b52d9d1610b2dac4079956d6bb8e84bd3656a6
Griff Gamble <griff@gm-charlie.com>
2026-08-11 16:54:54 +0800

    Docket v21

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v18';
+const CACHE = 'docket-v21';
 
 const SHELL = [
   './',
