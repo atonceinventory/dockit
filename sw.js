9380709c2111ba8f873d1e81e811e4169bc2082e
Griff Gamble <griff@gm-charlie.com>
2026-08-11 17:14:13 +0800

    Dockit v22 login landing and business hubs

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v21';
+const CACHE = 'docket-v22';
 
 const SHELL = [
   './',
