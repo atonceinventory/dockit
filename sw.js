1fb370166077d2a091bd74b123e6eb15c2751c7a
Griff Gamble <griff@gm-charlie.com>
2026-08-11 18:02:12 +0800

    trim logo + v31 logo sizing

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v30';
+const CACHE = 'docket-v31';
 
 const SHELL = [
   './',
