960952028ef6416d8b323faf11564ba17ce14a5d
Griff Gamble <griff@gm-charlie.com>
2026-08-12 13:44:53 +0800

    Dockit v47 scale back and align

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v46';
+const CACHE = 'docket-v47';
 
 const SHELL = [
   './',
