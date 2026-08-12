1dcafe6e0ed2c3d0117a8edc7472d4fcf5ad2e53
Griff Gamble <griff@gm-charlie.com>
2026-08-12 13:46:07 +0800

    Dockit v48 D icon and layout fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v47';
+const CACHE = 'docket-v48';
 
 const SHELL = [
   './',
