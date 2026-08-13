58e2f84cccb806509b964fc1e4d878d12a61ac78
Griff Gamble <griff@gm-charlie.com>
2026-08-13 11:29:34 +0800

    Docket v60 gmc logo and cross-search dedupe

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v59';
+const CACHE = 'docket-v60';
 
 const SHELL = [
   './',
