87a4aeb55411912d0b6e7fa54f606c73917902b8
Griff Gamble <griff@gm-charlie.com>
2026-08-13 10:12:18 +0800

    Docket v57 star removes job from alerts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Docket service worker.
    Bump CACHE when you change index.html so phones pick up the new version. */
-const CACHE = 'docket-v56';
+const CACHE = 'docket-v57';
 
 const SHELL = [
   './',
